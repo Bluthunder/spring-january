d140b8d54e215ccd8b6737e697171e941beece6f
kaushiktd <kaushiktd@gmail.com>
2024-02-20 17:35:30 +0530

    tests updated

--- src/test/java/com/example/springgha/SpringGhaApplicationTests.java
+++ src/test/java/com/example/springgha/SpringGhaApplicationTests.java
@@ -2,10 +2,13 @@ package com.example.springgha;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 class SpringGhaApplicationTests {
 
+
+	@Profile("local-test")
 	@Test
 	void contextLoads() {
 	}
